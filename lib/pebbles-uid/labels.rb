80c7e76da9a97295f64016086812b44b5b4125d3
Katrina Owen <katrina.owen@gmail.com>
2012-09-05 18:51:06 +0200

    Make labels more flexible

--- lib/pebbles-uid/labels.rb
+++ lib/pebbles-uid/labels.rb
@@ -1,10 +1,23 @@
 module Pebbles
   class Uid
     class Labels
+      include Enumerable
 
       attr_reader :values, :prefix
       def initialize(*values)
-        @values = values.map {|v| v.split('.') }.flatten
+        @values = values.flatten.map {|v| v.split('.') }.flatten
+      end
+
+      def each
+        values.each { |value| yield value }
+      end
+
+      def tail
+        values[1..-1]
+      end
+
+      def size
+        values.size
       end
 
       def to_a
--- spec/labels_spec.rb
+++ spec/labels_spec.rb
@@ -7,9 +7,19 @@ describe Pebbles::Uid::Labels do
   its(:to_s) { should eq('a.b.c') }
   its(:to_a) { should eq(%w(a b c)) }
   its(:to_hash) {  should eq('label_0' => "a", 'label_1' => "b", 'label_2' => "c") }
+  its(:tail) { should eq(%w(b c)) }
 
   it "delegates to conditions" do
     subject.to_hash(:name => 'thing', :verbose => false).should eq({'thing' => 'a.b.c'})
+  end
+
+  it "has a size" do
+    subject.size.should eq(3)
+  end
+
+  describe "alternate constructors" do
+    it { Pebbles::Uid::Labels.new('a', 'b', 'c').to_s.should eq('a.b.c') }
+    it { Pebbles::Uid::Labels.new(['a', 'b', 'c']).to_s.should eq('a.b.c') }
   end
 
 end
