584b0623941048aae696886af02c6c449c50a513
Katrina Owen <katrina.owen@gmail.com>
2012-09-27 11:21:06 +0200

    Implement wildcard paths

---
+++ lib/pebbles-uid/wildcard.rb
@@ -0,0 +1,45 @@
+module Pebbles
+  class Uid
+    module Wildcard
+
+      class << self
+
+        def valid?(path)
+          stars_are_solitary?(path) && single_caret?(path) && pipes_are_interleaved?(path) && carets_are_leading?(path) && stars_are_terminating?(path)
+        end
+
+        # a.*.c passes
+        # a.*b.c does not
+        # A later rule ensures that * always falls at the end of a path
+        def stars_are_solitary?(path)
+          !path.split('.').any? {|s| s.match(/.+\*|\*.+/)}
+        end
+
+        # a.b|c.d passes
+        # a.|b.c does not
+        def pipes_are_interleaved?(path)
+          !path.split('.').any? {|s| s.match(/^\||\|$/)}
+        end
+
+        # a.^b.c passes
+        # a.^c.^d does not
+        def single_caret?(path)
+          path.split('.').select {|s| s.match(/\^/) }.size <= 1
+        end
+
+        # a.^b.c passes
+        # a.b^c.d does not
+        def carets_are_leading?(path)
+          !path.split('.').any? {|s| s.match(/.+\^|\^$/) }
+        end
+
+        # a.b.* passes
+        # *.b.c does not
+        def stars_are_terminating?(path)
+          path !~ /.*\*\.\w/
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/wildcard_spec.rb
@@ -0,0 +1,53 @@
+require 'pebbles-uid/wildcard'
+
+describe Pebbles::Uid::Wildcard do
+
+  # This might not actually be necessary/relevant
+  specify "non-wildcard paths are valid" do
+    Pebbles::Uid::Wildcard.valid?('a.b.d').should == true
+  end
+
+  context "terminating asterisk representing entire label" do
+    [
+      '*', 'a.b.c.*', 'a.b|c.*', 'a.^b.c.*'
+    ].each do |path|
+      specify "#{path} is valid" do
+        Pebbles::Uid::Wildcard.valid?(path).should == true
+      end
+    end
+
+    ['*a', 'a*', '*.b', 'a.*.b'].each do |path|
+      specify "#{path} is invalid" do
+        Pebbles::Uid::Wildcard.valid?(path).should == false
+      end
+    end
+  end
+
+  context "pipes separate labels" do
+    ['a|b', 'a.b|c.d', 'a|b.c|d'].each do |path|
+      specify "#{path} is valid" do
+        Pebbles::Uid::Wildcard.valid?(path).should == true
+      end
+    end
+
+    ['|', 'a.|b', 'a.b|', 'a.|b.c', 'a.b|.c'].each do |path|
+      specify "#{path} is invalid" do
+        Pebbles::Uid::Wildcard.valid?(path).should == false
+      end
+    end
+  end
+
+  context "carets start an ancestry chain" do
+    ['^a', '^a.b', 'a.^b.c.*'].each do |path|
+      specify "#{path} is valid" do
+        Pebbles::Uid::Wildcard.valid?(path).should == true
+      end
+    end
+
+    ['^', '^.a', 'a^', 'a^b.c', '^a.^b'].each do |path|
+      specify "#{path} is invalid" do
+        Pebbles::Uid::Wildcard.valid?(path).should == false
+      end
+    end
+  end
+end
