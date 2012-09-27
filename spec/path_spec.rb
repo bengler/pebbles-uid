fa4b33b0c090ce733467bf99adeafb51587a299d
Katrina Owen <katrina.owen@gmail.com>
2012-09-27 13:45:03 +0200

    Refactor path specs

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -10,20 +10,9 @@ describe Pebbles::Uid::Path do
   its(:to_s) { should eq('magical.forrest.clearing') }
   its(:to_a) { should eq(%w(magical forrest clearing)) }
   its(:to_hash) { should eq({'path_0' => 'magical', 'path_1' => 'forrest', 'path_2' => 'clearing'}) }
-  its(:valid?) { should be_true }
 
   it "can customize the hash" do
     subject.to_hash(:name => 'label').should eq({'label_0' => 'magical', 'label_1' => 'forrest', 'label_2' => 'clearing'})
   end
-
-  specify { Pebbles::Uid::Path.new("abc123").should be_valid }
-  specify { Pebbles::Uid::Path.new("abc.123").should be_valid }
-  specify { Pebbles::Uid::Path.new("abc.de-f.123").should be_valid }
-
-  specify { Pebbles::Uid::Path.new("").should_not be_valid }
-  specify { Pebbles::Uid::Path.new(".").should_not be_valid }
-  specify { Pebbles::Uid::Path.new("..").should_not be_valid }
-  specify { Pebbles::Uid::Path.new("abc!").should_not be_valid }
-  specify { Pebbles::Uid::Path.new("ab. 123").should_not be_valid }
 
 end
