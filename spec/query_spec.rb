0bc0829123932d9aaad42f06bcadffdeee47a6ae
Katrina Owen <katrina.owen@gmail.com>
2012-10-01 10:08:46 +0200

    Think out loud about queries

--- spec/query_spec.rb
+++ spec/query_spec.rb
@@ -2,4 +2,35 @@ require 'pebbles-uid'
 
 describe Pebbles::Uid::Query do
 
+  context "for a single resource" do
+    it "is not a list"
+    it "is not a collection"
+
+    it "handles a full uid"
+    it "handles a wildcard path if realm is given"
+
+    it "bails without realm"
+    it "bails with unspecific species"
+    it "bails with unspecified oid"
+  end
+
+  context "for a list of resources" do
+    it "is not for a single resource"
+    it "is not a collection"
+
+    it "handles a comma separated list of single-resource uids"
+    it "handles a pipe-delimited list of oids"
+
+    it "bails without realm"
+    it "bails with unspecific species"
+    it "bails with unspecified oid"
+  end
+
+  context "for a collection of resources" do
+    it "is not for a single resource"
+    it "is not a list"
+
+    it "does all sorts of crazy-complicated wildcard stuff"
+  end
+
 end
