84da81a5bce01bd0c95a1259f704ac88aa243d18
Katrina Owen <katrina.owen@gmail.com>
2012-10-04 07:26:00 +0200

    Rename leftover species -> genus

--- lib/pebbles-uid/query.rb
+++ lib/pebbles-uid/query.rb
@@ -54,29 +54,29 @@ module Pebbles
 
       def wildcard_query?
         return false if term.include?(',')
-        species, _, oid = Pebbles::Uid.parse(term)
-        return true if Genus.new(species).wildcard?
+        genus, _, oid = Pebbles::Uid.parse(term)
+        return true if Genus.new(genus).wildcard?
         return true if oid.nil? || oid.empty? || oid == '*'
         false
       end
 
       def extract_terms
         term.split(',').map do |uid|
-          species, path, oid = Pebbles::Uid.parse(uid)
-          species_labels = Genus.new(species)
-          path_labels = Path.new(path)
-          oid_box = Oid.new(oid)
+          _genus, _path, _oid = Pebbles::Uid.parse(uid)
+          genus_labels = Genus.new(_genus)
+          path_labels = Path.new(_path)
+          oid_box = Oid.new(_oid)
 
           raise ArgumentError.new('Realm must be specified') unless path_labels.realm?
-          raise ArgumentError.new('Genus must unambiguous') if species_labels.ambiguous?
+          raise ArgumentError.new('Genus must unambiguous') if genus_labels.ambiguous?
           raise ArgumentError.new('Oid must be unambiguous') if oid_box.ambiguous?
 
           @realm ||= path_labels.realm
           raise ArgumentError.new('One realm at a time, please') if @realm != path_labels.realm
 
           if oid_box.multiple?
-            oid.split('|').map do |s|
-              "#{species}:#{path}$#{s}"
+            _oid.split('|').map do |s|
+              "#{_genus}:#{_path}$#{s}"
             end
           else
             uid
