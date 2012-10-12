795e0ae5313a691e93e22c3030559ad803f6d73a
Katrina Owen <katrina.owen@gmail.com>
2012-10-12 13:15:19 +0200

    Refactor uid specs for readability

--- spec/uid_spec.rb
+++ spec/uid_spec.rb
@@ -4,16 +4,6 @@ describe Pebbles::Uid do
 
   let(:uid) { 'post.card:tourism.norway.fjords$1234' }
 
-  describe "extracting single elements" do
-    specify { Pebbles::Uid.oid(uid).should eq('1234') }
-    specify { Pebbles::Uid.path(uid).should eq('tourism.norway.fjords') }
-    specify { Pebbles::Uid.genus(uid).should eq('post.card') }
-
-    describe "with missing oid" do
-      specify { Pebbles::Uid.oid('post:a.b.c').should eq(nil) }
-    end
-  end
-
   describe "query" do
     it "returns a query object" do
       s = 'post:tourism.*$*'
@@ -22,56 +12,56 @@ describe Pebbles::Uid do
     end
   end
 
-  describe "requirements" do
-    it "must have a genus" do
+  describe "extracts elements" do
+    specify "genus" do
+      Pebbles::Uid.genus(uid).should eq('post.card')
+    end
+
+    specify "path" do
+      Pebbles::Uid.path(uid).should eq('tourism.norway.fjords')
+    end
+
+    specify "oid" do
+      Pebbles::Uid.oid(uid).should eq('1234')
+    end
+
+    specify "non-existant oid" do
+      Pebbles::Uid.oid('post:a.b.c').should be_nil
+    end
+  end
+
+  describe "must" do
+    specify "have a genus" do
       ->{ Pebbles::Uid.new(':tourism.norway$1') }.should raise_error(ArgumentError)
     end
 
-    it "must have a realm" do
+    specify "have a realm" do
       ->{ Pebbles::Uid.new('post:$1') }.should raise_error(ArgumentError)
     end
+  end
 
-    it "can skip the oid" do
+  describe "may" do
+    specify "have no oid" do
       ->{ Pebbles::Uid.new('post:tourism') }.should_not raise_error
     end
+  end
 
-    it "doesn't accept wildcard genus" do
+  describe "rejects" do
+    it "wildcard genus" do
       ->{ Pebbles::Uid.new('post.*:tourism$1') }.should raise_error(ArgumentError)
       ->{ Pebbles::Uid.new('post|card:tourism$1') }.should raise_error(ArgumentError)
       ->{ Pebbles::Uid.new('post.^b.c:tourism$1') }.should raise_error(ArgumentError)
     end
 
-    it "doesn't accept wildcard paths" do
+    it "wildcard paths" do
       ->{ Pebbles::Uid.new('post:tourism.*$1') }.should raise_error(ArgumentError)
       ->{ Pebbles::Uid.new('post:tourism|blogging$1') }.should raise_error(ArgumentError)
       ->{ Pebbles::Uid.new('post:tourism.^b.c$1') }.should raise_error(ArgumentError)
     end
 
-    it "doesn't accept wildcard oid" do
+    it "wildcard oid" do
       ->{ Pebbles::Uid.new('post:tourism$*') }.should raise_error(ArgumentError)
     end
-  end
-
-  subject { Pebbles::Uid.new(uid) }
-
-  its(:to_s) { should eq(uid) }
-  its(:realm) { should eq('tourism') }
-  its(:genus) { should eq('post.card') }
-  its(:species) { should eq('card') }
-  its(:path) { should eq('tourism.norway.fjords') }
-  its(:oid) { should eq('1234') }
-  its(:oid?) { should == true }
-
-  its(:cache_key) { should eq('post.card:tourism.*$1234') }
-
-  context "when pending creation" do
-
-    let(:uid) { 'post.doc:universities.europe.norway' }
-    subject { Pebbles::Uid.new(uid) }
-
-    its(:to_s) { should eq(uid) }
-    its(:oid) { should be_nil }
-
   end
 
   context "paths" do
@@ -103,9 +93,6 @@ describe Pebbles::Uid do
   end
 
   context "oids" do
-    it "can be empty" do
-      Pebbles::Uid.new("beast:mythical").oid?.should == false
-    end
 
     it "cannot contain pipes" do
       Pebbles::Uid.valid_oid?("abc|xyz").should == false
@@ -125,6 +112,30 @@ describe Pebbles::Uid do
 
     it "is a black box" do
       Pebbles::Uid.valid_oid?("holy+%^&*s!").should == true
+    end
+  end
+
+  describe "A Uid" do
+    subject { Pebbles::Uid.new(uid) }
+
+    its(:to_s) { should eq(uid) }
+    its(:realm) { should eq('tourism') }
+    its(:genus) { should eq('post.card') }
+    its(:species) { should eq('card') }
+    its(:path) { should eq('tourism.norway.fjords') }
+    its(:oid) { should eq('1234') }
+    its(:oid?) { should == true }
+
+    its(:cache_key) { should eq('post.card:tourism.*$1234') }
+
+    context "when pending creation" do
+
+      let(:uid) { 'post.doc:universities.europe.norway' }
+      subject { Pebbles::Uid.new(uid) }
+
+      its(:to_s) { should eq(uid) }
+      its(:oid) { should be_nil }
+
     end
   end
 end
